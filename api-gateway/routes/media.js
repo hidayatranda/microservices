395468d29155419f136ebf15e0f24067a370070c
hidayatranda <srcgolang@gmail.com>
2021-04-21 16:11:47 +0700

    integrasi api gateway

--- api-gateway/routes/media.js
+++ api-gateway/routes/media.js
@@ -5,5 +5,8 @@ const mediaHandler = require('./handler/media');
 
 
 router.post('/', mediaHandler.create);
+router.get('/', mediaHandler.getAll);
+router.delete('/:id', mediaHandler.destroy);
 
 module.exports = router;
+
